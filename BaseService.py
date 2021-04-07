2d8f299831b239c75485f2c12faffe9dd12eda87
Adel Hassan (ext) GBEX <hassan.adel@syngenta.com>
2021-04-07 13:05:20 +0300

    add root path

--- BaseService.py
+++ BaseService.py
@@ -7,13 +7,15 @@ from threading import Thread
 from multiprocessing import Process, process
 import json
 import sys
+import os
+
+config_dir = os.path.join(os.getcwd(), "config.json")
 
 class ScheduleTask(Thread):
   _nextRun = datetime.now()
   defaultSchedule = "*/5 * * * *"
   _isDisabled = False
   cron = None
-
   def __init__(self, cronExp):
     Thread.__init__(self)
     if(cronExp == None or cronExp == ""):
@@ -44,34 +46,34 @@ class ScheduleTask(Thread):
 
 class SampleService1(ScheduleTask, Thread):
   def __init__(self):
-    with open('D:\\config.json') as configSchema:
+    with open(config_dir) as configSchema:
       config = json.load(configSchema)
     
     ScheduleTask.__init__(self, "*/1 * * * *")
 
   
   def process(self):
-    with open('D:\\config.json') as configSchema:
+    with open(config_dir) as configSchema:
       config = json.load(configSchema)
     if(config["service1"]["status"] == "stop"):
       return
 
     _now = datetime.now()
     print('service 1 ' + self.getName() + ' -> ' + _now.strftime("%m/%d/%Y, %H:%M")+ "\n")
-    f = open("D:\\python_services_log\\service1.txt", "a")
+    f = open("service1.txt", "a")
     f.write('service 1 ' + self.getName() + ' -> ' + _now.strftime("%m/%d/%Y, %H:%M")+ "\n")
     f.close()
 
 
 class SampleService2(ScheduleTask, Thread):
   def __init__(self):
-    with open('D:\\config.json') as configSchema:
+    with open(config_dir) as configSchema:
       config = json.load(configSchema)
     ScheduleTask.__init__(self, config["service2"]["schedule"])
 
   
   def process(self):
-    with open('D:\\config.json') as configSchema:
+    with open(config_dir) as configSchema:
       config = json.load(configSchema)
     if(config["service2"]["status"] == "stop"):
       return
@@ -81,20 +83,20 @@ class SampleService2(ScheduleTask, Thread):
     except:
       print('service 2 error')
       _now = datetime.now()
-      f = open("D:\\python_services_log\\service2.txt", "a")
+      f = open("service2.txt", "a")
       f.write('service 2 error' + ' -> ' + _now.strftime("%m/%d/%Y, %H:%M")+ "\n")
       f.close()
 
 
 class SampleService3(ScheduleTask, Thread):
   def __init__(self):
-    with open('D:\\config.json') as configSchema:
+    with open(config_dir) as configSchema:
       config = json.load(configSchema)
     ScheduleTask.__init__(self, config["service3"]["schedule"])
 
   
   def process(self):
-    with open('D:\\config.json') as configSchema:
+    with open(config_dir) as configSchema:
       config = json.load(configSchema)
     if(config["service3"]["status"] == "stop"):
       return        
--- test.py
+++ test.py
@@ -1,10 +1,16 @@
 import croniter
 import datetime
+import os
 
 if __name__ == '__main__':
   now = datetime.datetime.now()
   cron = croniter.croniter('* * * * *', now)
   next1_ = cron.get_next(datetime.datetime)
+  goal_dir = os.path.join(os.getcwd(), "config.json")
+  print(goal_dir)  # prints C:/here/I/am/../../my_dir
+  print(os.path.normpath(goal_dir))  # prints C:/here/my_dir
+  print(os.path.realpath(goal_dir))  # prints C:/here/my_dir
+  print(os.path.abspath(goal_dir))  # prints C:/here/my_dir
   print(next1_)
   # datetime.datetime(2011, 9, 14, 17, 45)
   # cron.get_next(datetime.datetime)
